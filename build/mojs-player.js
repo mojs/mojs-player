d9925c03600cb0033bf93f4e91cbbe32f7b1aec9
Oleg Solomka <legomushroom@gmail.com>
2016-04-01 22:28:01 +0200

    package.json: add webpack dependencies

--- build/mojs-player.js
+++ build/mojs-player.js
@@ -44,7 +44,9 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+	__webpack_require__(1);
+	(function webpackMissingModule() { throw new Error("Cannot find module \"dev\""); }());
+	(function webpackMissingModule() { throw new Error("Cannot find module \"server\""); }());
 
 
 /***/ },
